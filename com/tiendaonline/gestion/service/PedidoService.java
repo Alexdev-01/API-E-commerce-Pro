cf53289b835c0c7f04fdf60655bb1594cfe3465e
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-19 16:09:19 +0200

    Add files via upload

--- com/tiendaonline/gestion/service/PedidoService.java
+++ com/tiendaonline/gestion/service/PedidoService.java
@@ -3,6 +3,7 @@ package com.tiendaonline.gestion.service;
 import java.util.List;
 
 import com.tiendaonline.gestion.dto.pedido.CrearPedidoRequest;
+import com.tiendaonline.gestion.dto.pedido.PedidoResponse;
 import com.tiendaonline.gestion.model.Pedido;
 import com.tiendaonline.gestion.model.Usuario;
 
@@ -10,9 +11,9 @@ public interface PedidoService {
 	
 	Pedido crearPedido(Pedido pedido);
 	
-	List<Pedido> obtenerPedidosUsuario(Usuario usuario);
+	List<PedidoResponse> obtenerPedidosUsuario(Usuario usuario);
 	
-	List<Pedido> obtenerTodos();
+	List<PedidoResponse> obtenerTodos();
 	
 	Pedido crearPedido(CrearPedidoRequest request, String username);
 
