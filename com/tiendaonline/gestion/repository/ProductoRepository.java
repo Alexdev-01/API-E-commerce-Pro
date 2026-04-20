13004a6119750f342fae2eb1ed982b713733daee
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-20 15:15:53 +0200

    Add files via upload

--- com/tiendaonline/gestion/repository/ProductoRepository.java
+++ com/tiendaonline/gestion/repository/ProductoRepository.java
@@ -2,9 +2,13 @@ package com.tiendaonline.gestion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 import com.tiendaonline.gestion.model.Producto;
 
 public interface ProductoRepository  extends JpaRepository<Producto, Long>, JpaSpecificationExecutor<Producto> {
 
+	Page<Producto> findByActivoTrue(Pageable pageable);
 }
